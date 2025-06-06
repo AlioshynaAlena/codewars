dace6de12adb03d9fb7a425a8873e7220a2be7b9
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-06-06 19:14:49 +0300

    boolToWord

---
+++ 8kyu/сonvert-boolean-values.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// 🏓 TESTS
+// assert.strictEqual(boolToWord(true), 'Yes')
+// assert.strictEqual(boolToWord(false), 'No')
+
+
+// ✅SOLUTION
+function boolToWord(bool) {
+    return bool ? "Yes" : "No"
+}
+console.log(boolToWord(false));
