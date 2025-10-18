b1498a34facb058f998eed8450705cd8ff4e4b1a
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-10-18 21:05:13 +0300

    noSpace

---
+++ 8kyu/remove-string-spaces.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+
+// 🏓 TESTS
+//   testIt('8 j 8   mBliB8g  imjB8B8  jl  B', '8j8mBliB8gimjB8B8jlB');
+//   testIt('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd', '88Bifk8hB8BB8BBBB888chl8BhBfd');
+//   testIt('8aaaaa dddd r     ', '8aaaaaddddr');
+
+// ✅SOLUTION
+function noSpace(x) {
+    return x.replaceAll(" ", "")
+}
+
+
+console.log(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'));
