ca0766d87e3b3fc401b6aaac054d0f816ebbb1ba
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-10-31 18:39:27 +0300

    likes

---
+++ 6 kyu/who-likes-it.js
@@ -0,0 +1,39 @@
+// ❓DESCRIPTION
+// Given an array of integers, find the one that appears an odd number of times.
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//   Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+//   []                                -->  "no one likes this"
+//     ["Peter"]                         -->  "Peter likes this"
+//     ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+//     ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+//     ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+// 🏓 TESTS
+// assert.strictEqual(likes([]), 'no one likes this');
+// assert.strictEqual(likes(['Peter']), 'Peter likes this');
+// assert.strictEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+// assert.strictEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+// assert.strictEqual(likes(['Alex', 'Jacob', 'Mark', 'Max', 'Max', 'Max',
+// 'Max', 'Max']),
+// 'Alex, Jacob
+// and 2 others like this');
+
+// ✅SOLUTION
+function likes(names) {
+
+    if (names.length === 1) {
+      return `${names[0]} likes this`
+    }
+    if (names.length === 2) {
+      return `${names[0]} and ${names[1]} like this`
+    }
+    if (names.length === 3) {
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    }
+    if (names.length >= 4) {
+      return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+    }
+  return `no one likes this`
+}
+
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max', 'Max', 'Max', 'Max', 'Max']))
