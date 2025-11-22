0e62808dfbc7bc04d21896ba988ce7e7aa805b80
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-11-22 21:05:31 +0300

    maps

---
+++ 8kyu/Beginner-Lost-Without-a-Map.js
@@ -0,0 +1,19 @@
+// ❓DESCRIPTION
+// Given an array of integers, return a new array with each value doubled.
+//   For example:
+//   [1, 2, 3] --> [2, 4, 6]
+
+// 🏓 TESTS
+// assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+// assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]);
+// assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]);
+
+
+
+
+// ✅SOLUTION
+function maps(x){
+  return x.map(x => x+x)
+}
+
+console.log(maps([4, 1, 1, 1, 4]))
